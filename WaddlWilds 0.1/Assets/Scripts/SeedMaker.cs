427d4c36553644a230de6cd322599438936b6bf7
SlabOfRocks <jakejin708@gmail.com>
2026-08-03 17:11:47 -0400

    Tweaks
    Tweaked some parameters and added a main menu

--- WaddlWilds 0.1/Assets/Scripts/SeedMaker.cs
+++ WaddlWilds 0.1/Assets/Scripts/SeedMaker.cs
@@ -13,7 +13,7 @@ public class SeedMaker : MonoBehaviour
 
     void Start()
     {
-        seeds += Random.Range(-randomizer, randomizer);
+        seeds += Random.Range(-randomizer, randomizer+1);
         player = GameObject.FindWithTag("Player");
     }
 
@@ -23,7 +23,7 @@ public class SeedMaker : MonoBehaviour
         float dist = Mathf.Pow(Mathf.Pow(Mathf.Abs(player.transform.position.x-transform.position.x), 2f) + Mathf.Pow(Mathf.Abs(player.transform.position.y-transform.position.y), 2f), 0.5f);
 
         if (dist < pickDist) {
-            if (Input.GetKey(KeyCode.F) && seeds > 0) {
+            if (Input.GetKeyDown(KeyCode.F) && seeds > 0) {
                 for(int i = 0; i < seeds; i++) {
                     Instantiate(loot, new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z), transform.rotation);
                 }
