2a6d499a3ef0eae3b569923bfae87ecfe964b9b3
SlabOfRocks <jakejin708@gmail.com>
2026-08-03 13:47:40 -0400

    Fixed Cam follow script
    yeah

--- WaddlWilds 0.1/Assets/Scripts/CameraFollow.cs
+++ WaddlWilds 0.1/Assets/Scripts/CameraFollow.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float speedMod = 0.01;
+    public float speedMod = 0.01f;
     public GameObject player;
     public GameObject cameraZone;
     
     void LateUpdate()
     {
-        float newX;
-        float newY;
+        float newX = transform.position.x;
+        float newY = transform.position.y;
         if(player != null) {
             float distX = player.transform.position.x - transform.position.x;
             float distY = player.transform.position.y - transform.position.y;
@@ -19,17 +19,19 @@ public class CameraFollow : MonoBehaviour
             newY = transform.position.y + distY*speedMod;
         }
 
-        BoxCollider2D bcd = cameraZone.GetComponent<BoxCollider2D>();
+        if (cameraZone != null) {
+            BoxCollider2D bcd = cameraZone.GetComponent<BoxCollider2D>();
 
-        xRange = bcd.size.x / 2f;
-        yRange = bcd.size.y / 2f;
+            float xRange = bcd.size.x / 2f;
+            float yRange = bcd.size.y / 2f;
 
-        newX = Mathf.Min(newX, cameraZone.transform.position.x + distX);
-        newX = Mathf.Max(newX, cameraZone.transform.position.x - distX);
-        newY = Mathf.Min(newX, cameraZone.transform.position.y + distY);
-        newY = Mathf.Max(newX, cameraZone.transform.position.y - distY);
+            newX = Mathf.Min(newX, cameraZone.transform.position.x + xRange);
+            newX = Mathf.Max(newX, cameraZone.transform.position.x - xRange);
+            newY = Mathf.Min(newX, cameraZone.transform.position.y + yRange);
+            newY = Mathf.Max(newX, cameraZone.transform.position.y - yRange);
 
-        transform.position = new Vector3(newX, newY, transform.position.z);
+            transform.position = new Vector3(newX, newY, transform.position.z);
+        }
     }
     public void shake(float heavyness) {
         transform.position = new Vector3(transform.position.x + Random.Range(-heavyness, heavyness), transform.position.y + Random.Range(-heavyness, heavyness), transform.position.z);
